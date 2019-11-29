f6143dd03a53e4ec83c21b6346efa1ee0edecf07
Aaron Angle <acangle10@gmail.com>
2019-11-29 15:20:37 -0700

    Backend Routes

--- models/example.js
+++ models/example.js
@@ -1,7 +1,7 @@
 module.exports = function (sequelize, DataTypes) {
   var Review = sequelize.define("Review", {
-    movieID: DataTypes.STRING,
-    Rating: DataTypes.TEXT,
+    movieID: DataTypes.INTEGER,
+    Rating: DataTypes.INTEGER,
     Review: DataTypes.STRING
   });
   return Review;
